989eb5fc7e5d3657578a816d7a50f8fa02e8d44f
Vuth Menghuor <hourousa9@gmail.com>
2026-02-01 22:12:50 +0700

    changed grid-cols-2 to grid-cols-1 for responsive on mobile

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -67,7 +67,7 @@ const Projects = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {projects.map((project) => (
             <div
               key={project.id}
